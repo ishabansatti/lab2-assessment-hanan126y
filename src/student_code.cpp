53ffb0b51da5dcf45c4cb62f45194ffd72e62014
hanan126y <ha2073220@gmail.com>
2026-02-18 17:40:39 +0500

    Refactor variable declarations and fix conversion logic
    Fixed variable declarations and return statements. Corrected the conversion formula in fahrenheitToCelsius.

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -9,17 +9,21 @@ int getIntValue() {
 
 float getFloatValue() {
     // Declare a variable var_float of type float and set its value to 10.75.
-    //this is a code
+    float var_float = 10.75;
     //return var_float;
+    return var_float;
 }
 
 char getCharValue() {
     // Declare a variable var_char of type character and set its value to A.
+    char var_char = 'A';
     //return var_char;
+    return var_char;
 }
 
 int convertFloatToInt(float value) {
     // convert value to int.
+    static_cast(int)<<value<<endl;
     return value;
 }
 
@@ -30,6 +34,8 @@ Formula:
     Celsius = (Fahrenheit - 32) * 5 / 9
 */
 float fahrenheitToCelsius(float fahrenheit) {
+    Celsius = (Fahrenheit - 32) * 5 / 9;
     //return celsius;
+    return celsius;
 
 }
